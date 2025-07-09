89980fb29638f5d3fd6eafe6df04586b59e2868d
Raya.P.shopifydev <rayapetkova2007@abv.bg>
2025-07-09 11:40:57 +0300

    Add lesson 1

---
+++ lesson_1/task_3.py
@@ -0,0 +1,7 @@
+hero = "Arisu"
+animal = "dog"
+city = "Tokyo"
+
+print(hero)
+print(animal)
+print(city)
---
+++ lesson_1/task_4.py
@@ -0,0 +1,10 @@
+name = "Рая"
+age = 18
+favourite_game = "Minecraft"  # Snake case
+
+# print("Казвам се", name)
+print(f"Казвам се {name}.")  # предпочитан вариант за принтиране на стринг с променлива
+
+print(f"Аз съм на {age} години.")
+
+print(f"Моята любима игра е {favourite_game}.")
