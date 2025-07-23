307dc1e4c1fb64f67593c5934b826c7f4346d147
Raya Petkova <rayapetkova2007@abv.bg>
2025-07-23 11:14:32 +0300

    Add lesson_2 - examples and tasks

---
+++ lesson_2/examples.py
@@ -0,0 +1,16 @@
+# name = input("Как се казваш?: ")
+# print(name)
+#
+# age = int(input("На колко си години?: "))
+# print(age)
+#
+# print(f"Аз се казвам {name} и съм на {age} години.")
+
+age = 20
+
+if age < 13:
+    print("дете")
+elif 13 <= age <= 18:
+    print("тийнейджър")
+else:
+    print("възрастен")
---
+++ lesson_2/task_1.py
@@ -0,0 +1,4 @@
+name = input("Как се казваш?: ")
+favourite_animal = input("Кое е любимото ти животно?: ")
+
+print(f"Аз се казвам {name}. Моето любимо животно е {favourite_animal}.")
